ba00e78091b558a9786e7fe3f8436fdcbfbc27e1
Wallace Basso <atendimentowallacebasso@gmail.com>
2025-08-08 05:44:49 +0000

    troque o link do botao para: https://wallacebasso.com.br/zap-raspa-venda

--- src/app/conexao/page.tsx
+++ src/app/conexao/page.tsx
@@ -6,7 +6,7 @@ import { BadgeCheck } from 'lucide-react';
 import Image from 'next/image';
 
 export default function ConexaoPage() {
-  const whatsappLink = 'https://wallacebasso.com.br/zap-raspa.html';
+  const whatsappLink = 'https://wallacebasso.com.br/zap-raspa-venda.html';
 
   const handleButtonClick = () => {
     window.location.href = whatsappLink;
