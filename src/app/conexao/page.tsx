72ffdcca3a4e1ba85610e586ff5c59721b4d37a1
Wallace Basso <atendimentowallacebasso@gmail.com>
2025-11-07 02:28:03 +0000

    Em "plataforma em 24h" e "pague após a entrega" deixe o fundo da caixa d

--- src/app/conexao/page.tsx
+++ src/app/conexao/page.tsx
@@ -75,7 +75,7 @@ export default function ConexaoPage() {
                 
                 <div className="space-y-3">
                   <h1 className="text-4xl font-black tracking-tighter sm:text-5xl md:text-6xl text-accent animate-fade-in-down">
-                    Tenha sua plataforma em 24h e pague após a entrega
+                    Tenha sua <span className="bg-red-600 text-white px-2 rounded-md">plataforma em 24h</span> e <span className="bg-red-600 text-white px-2 rounded-md">pague após a entrega</span>
                   </h1>
                 </div>
 
